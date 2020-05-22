1fe0473c837faa51267111b052d05965f54d4a60
BordListian <bordlistian@hotmail.de>
2020-05-22 05:10:15 +0200

    - Fixes engine hiding adjacent blocks. Closes #1

--- src/main/java/prodigymechanics/BlockHotAirEngine.java
+++ src/main/java/prodigymechanics/BlockHotAirEngine.java
@@ -5,6 +5,7 @@ import lykrast.prodigytech.common.capability.CapabilityHotAir;
 import lykrast.prodigytech.common.util.TemperatureHelper;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -13,11 +14,32 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockHotAirEngine extends BlockMachineActiveable<TileHotAirEngine> {
     public BlockHotAirEngine(float hardness, float resistance, int harvestLevel) {
         super(Material.IRON, SoundType.METAL, hardness, resistance, "pickaxe", harvestLevel, TileHotAirEngine.class);
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        if(face == EnumFacing.DOWN)
+            return BlockFaceShape.SOLID;
+        else if(face != null && face == state.getValue(FACING))
+            return BlockFaceShape.CENTER_BIG;
+        else
+            return BlockFaceShape.UNDEFINED;
     }
 
     public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
